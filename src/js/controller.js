5572fdb2eb3e6355211285e27ddd7964a7e27835
omerrbenarieh <omerrbenarieh@gmail.com>
2022-10-09 00:56:04 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,6 +120,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the App! :)');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -128,6 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
